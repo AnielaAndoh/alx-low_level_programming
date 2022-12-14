d1218034ae60f3d95b046671890d9b8954109ab4
AnielaAndoh <anielaandoh.com>
2022-12-14 12:46:32 -0800

    8 git commit -m 8-24_hours.c git commit -m 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,8 @@
 #include "main.h"
+
 /**
- *print every minute of the day of Jack Bauer,starting from 00:00 to 23:59
- *
- * 
- * Return: Always 0.
+ *jack_bauer - Prints every minute of the day of 
+ *	Jack Bauer,starting from 00:00 to 23:59
  */
 void jack_bauer(void)
 {
