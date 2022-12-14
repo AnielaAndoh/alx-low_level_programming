dcfd7e3502af695448abf4d6d4fa6dd77a8d9b02
AnielaAndoh <anielaandoh.com>
2022-12-14 02:41:47 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,14 @@
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('s');
-_putchar('t');
-_putchar('u');
-_putchar('a');
-_putchar('n');
-_putchar('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('s');
+	_putchar('t');
+	_putchar('u');
+	_putchar('a');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
 }
