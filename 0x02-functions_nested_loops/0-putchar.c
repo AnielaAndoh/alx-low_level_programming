38ae0fd7430fad0ae3088c01b0e3446acb3819aa
AnielaAndoh <anielaandoh.com>
2022-12-14 02:31:16 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main -print _putchar
+ * Return: 0
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('s');
+_putchar('t');
+_putchar('u');
+_putchar('a');
+_putchar('n');
+_putchar('\n');
+return (0);
+}
